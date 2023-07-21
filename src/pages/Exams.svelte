73303b647bb46bb4731e8d3dbb80b02b2bfc05cb
Kerollos Magdy <kerolloz@yahoo.com>
2023-07-21 18:48:19 +0300

    exams view

--- src/pages/Exams.svelte
+++ src/pages/Exams.svelte
@@ -6,21 +6,24 @@
 
 <div>
   <h1>Exams</h1>
-  <ul>
-    {#await exams}
-      <p>Loading exams...</p>
-    {:then exams}
+
+  {#await exams}
+    <p>Loading exams...</p>
+  {:then exams}
+    <div class="ui large relaxed divided list">
       {#each exams as exam}
-        <li>
-          <a href="/exams/{exam.id}">{exam.name}: {exam.duration}</a>
-        </li>
+        <div class="item">
+          <i class=" large code middle aligned icon" />
+          <div class="content">
+            <a class="header" href="/exams/{exam.id}">{exam.name}</a>
+            <div class="description">Duration: {exam.duration} Minutes</div>
+          </div>
+        </div>
       {/each}
-    {:catch error}
-      <p class="text-red-600 text-xl text-center">
-        Something went wrong.
-        <br /> Couldn't fetch articles!<br />
-        <span class="text-gray-500 text-sm">{error.message}</span>
-      </p>
-    {/await}
-  </ul>
+    </div>
+  {:catch error}
+    <p>Something went wrong.</p>
+    <p>Couldn't fetch Exams!</p>
+    <p>{error.message}</p>
+  {/await}
 </div>
